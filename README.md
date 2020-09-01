74f9233fec2be3b89006b7a09184f404eca0f214
iionly <iionly@gmx.de>
2020-09-01 22:07:21 +0200

    Updated to version 3.3.0; changes see CHANGES.txt

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-http:blacklist for Elgg 3.0 and newer Elgg 3.X
+http:blacklist for Elgg 3.3 and newer Elgg 3.X
 ==============================================
 
-Latest Version: 3.0.0  
-Released: 2019-06-07  
+Latest Version: 3.3.0  
+Released: 2020-08-29  
 Contact: iionly@gmx.de  
 License: GNU General Public License version 2  
 Copyright: (c) iionly
--- elgg-plugin.php
+++ elgg-plugin.php
@@ -11,4 +11,23 @@ return [
 		'httpblmaxthreat' => 25,
 		'counter' => 0,
 	],
+	'hooks' => [
+		'route:rewrite' => [
+			'register' => [
+				'\Elgg\HttpBlacklist\RouterHttpBlacklist\RouterHttpBlacklist::class' => [
+					'priority' => 100,
+				],
+			],
+			'login' => [
+				'\Elgg\HttpBlacklist\RouterHttpBlacklist\RouterHttpBlacklist::class' => [
+					'priority' => 101,
+				],
+			],
+			'forgotpassword' => [
+				'\Elgg\HttpBlacklist\RouterHttpBlacklist\RouterHttpBlacklist::class' => [
+					'priority' => 102,
+				],
+			],
+		],
+	],
 ];
